c545d0d5beaeb4d5b6fd9c4aa0aa323b1f394f0e
Piotr <piotrsiwiec.lfc@outlook.com>
2024-05-04 22:25:18 +0200

    cosmetic changes

--- RamzesGame/main.cpp
+++ RamzesGame/main.cpp
@@ -1,12 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "stb.cpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-#include "shader_s.h"
 #include <ft2build.h>
+
+#include "stb.cpp"
+#include "shader_s.h"
 
 #include "camera.h"
 #include "axis.h"
